b8316dfa1ddcd04fa5d0e2dace0886fe934bf231
miha-9111 <miha-9111@yandex.ru>
2022-01-25 01:50:26 +0300

    Navigation Reminder

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from "./signup/signup.component";
+import { SigninComponent } from "./signin/signin.component";
+
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: '', component: SigninComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
