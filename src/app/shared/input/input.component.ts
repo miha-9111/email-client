47263b800ced19b8cacd96fc6090d3e48ffedd78
miha-9111 <miha-9111@yandex.ru>
2022-01-26 23:19:35 +0300

    Customizing Input Type

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class InputComponent {
   @Input() label: string;
   @Input() control: any;
+  @Input() inputType: string // 'password'
 
   showErrors() {
     const { dirty, touched, errors } = this.control;
