6ca678dd072cdcf4bf6617ad73a5e2b35d36ef40
miha-9111 <miha-9111@yandex.ru>
2022-01-27 23:01:12 +0300

    Checking Auth Status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from "./auth/auth.service";
 import { BehaviorSubject } from "rxjs";
 
@@ -7,10 +7,14 @@ import { BehaviorSubject } from "rxjs";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   signedIn$: BehaviorSubject<boolean>;
 
   constructor(private authService: AuthService) {
     this.signedIn$ = this.authService.signedIn$;
+  }
+
+  ngOnInit() {
+    this.authService.checkAuth().subscribe(() => {});
   }
 }
