54f21c61db2e1b8ee6f1c21186b2dd5071ec8b4a
miha-9111 <miha-9111@yandex.ru>
2022-01-28 04:15:23 +0300

    Automated Signouts

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SignupComponent } from "./signup/signup.component";
 import { SigninComponent } from "./signin/signin.component";
+import { SignoutComponent } from "./signout/signout.component";
 
 const routes: Routes = [
+  { path: 'signout', component: SignoutComponent },
   { path: 'signup', component: SignupComponent },
   { path: '', component: SigninComponent }
 ];
