c6623dcf31ae5157795a2be304545754a581b4b0
miha-9111 <miha-9111@yandex.ru>
2022-01-29 05:40:59 +0300

    Implementing Our RxJs Solution

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { take, skipWhile } from "rxjs";
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService) {}
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.signedIn$.pipe(
+      skipWhile(value => value === null),
+      take(1)
+    );
   }
 }
