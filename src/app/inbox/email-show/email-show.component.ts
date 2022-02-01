9f08dc2a7bcbdde9032b35fc12ea308028b7a5b0
miha-9111 <miha-9111@yandex.ru>
2022-02-01 13:06:31 +0300

    Canceling Previous Email Requests

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { EmailService } from "../email.service";
+import { switchMap } from "rxjs";
 
 @Component({
   selector: 'app-email-show',
@@ -13,10 +14,19 @@ export class EmailShowComponent implements OnInit {
               private emailService: EmailService) {}
 
   ngOnInit() {
-    this.route.params.subscribe(({ id }) => {
-      this.emailService.getEmails().subscribe(email => {
+    // this.route.params.subscribe(({ id }) => {
+    //   this.emailService.getEmails().subscribe(email => {
+    //     console.log(email);
+    //   });
+    // });
+    this.route.params
+      .pipe(
+        switchMap(({ id }) => {
+          return this.emailService.getEmail(id);
+        })
+      )
+      .subscribe(email => {
         console.log(email);
       });
-    });
   }
 }
