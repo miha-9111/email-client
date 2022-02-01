5120c736f228c8fa211f9ee8aa278df7e4113354
miha-9111 <miha-9111@yandex.ru>
2022-02-01 11:50:00 +0300

    Extracting URL Params

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-email-show',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailShowComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.params.subscribe(({ id }) => {
+      // this.email.Service.getEmail(id);
+    });
   }
-
 }
